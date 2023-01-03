045bf2802de522f753e9aeaa9c919763663bdddd
mazurenka <an.drej@mail.ru>
2023-01-03 15:15:40 +0100

    js4 Victor

--- 4/main.js
+++ 4/main.js
@@ -1,4 +1,22 @@
 const todoLists = [
-    {id: 1, title: 'What to learn'},
-    {id: 2, title: 'What to buy'},
+    {
+        id: 1,
+        title: 'What to learn',
+    },
+    {
+        id: 2,
+        title: 'What to buy',
+    },
 ]
+
+const tasks = {
+    1: [
+        {name: 'HTML', isDone: true},
+        {name: 'CSS', isDone: false}
+    ],
+    2: [
+        {name: 'Meat', isDone: false},
+        {name: 'Fish', isDone: false}
+    ]
+}
+
